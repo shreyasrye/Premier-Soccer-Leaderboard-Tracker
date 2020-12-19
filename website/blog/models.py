458d7c2d9e3182029d578a3771e338a9912a9dbc
Joseph Ismael <joseph.a.ismael@gmail.com>
2020-12-19 09:30:43 -0800

    database

--- website/blog/models.py
+++ website/blog/models.py
@@ -5,13 +5,19 @@ from django.contrib.auth.models import User
 class Team(models.Model):
     name = models.CharField(max_length=100)
     creater = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class League(models.Model):
     name = models.CharField(max_length=100)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creater = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+class UserTeams:
+      user = models.ForeignKey(user, on_delete=models.CASCADE)
+      team =  models.ForeignKey(Team, on_delete=models.CASCADE)
