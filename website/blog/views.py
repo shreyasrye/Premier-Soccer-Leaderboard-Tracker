02aaa39635e9e21d8ee7cba4c9b47a913854004f
William Ly <ly.william02@gmail.com>
2020-12-19 16:19:14 -0500

    Updated home view.

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Team, UserTeams
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    if request.method == "POST":
+        print(request.POST)
+        if "create_team" in request.POST:
+            # do stuff to create the team
+            Team.objects.create(name=team1)
+        elif "add_match" in request.POST:
+            # do stuff to update team standings
+            print()
+        else:
+            # unexpected error
+            return HttpResponse("<h1>About</h1>")
+
+        team1 = request.POST["team1"]
+    return render(request, "blog/select_league.html")
 
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return HttpResponse("<h1>About</h1>")
+
