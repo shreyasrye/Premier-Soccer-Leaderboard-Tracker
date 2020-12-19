56d55b4608dcdf56eae3b54afb7b4ee56e64a8a0
William Ly <ly.william02@gmail.com>
2020-12-19 13:06:59 -0500

    Added new models.

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import League, Team, UserTeams
 
 # Register your models here.
+models = [League, Team, UserTeams]
+
+for model in models:
+    admin.site.register(model)
--- website/blog/models.py
+++ website/blog/models.py
@@ -2,22 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Team(models.Model):
-    name = models.CharField(max_length=100)
-    creater = models.ForeignKey(User, on_delete=models.CASCADE)
-    code = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 class League(models.Model):
     name = models.CharField(max_length=100)
-    creater = models.ForeignKey(User, on_delete=models.CASCADE)
-    code = models.CharField(max_length=100)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-class UserTeams:
-      user = models.ForeignKey(user, on_delete=models.CASCADE)
-      team =  models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    draws = models.IntegerField()
+    goals_for = models.IntegerField()
+    goals_against = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class UserTeams(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
