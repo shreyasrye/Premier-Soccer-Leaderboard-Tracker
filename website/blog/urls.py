929a2543645bc84ecf0bd713c3fab26176c439e9
William Ly <ly.william02@gmail.com>
2020-12-19 19:22:21 -0500

    commit

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="blog-home"),
+    path("league/<int:league_id>/", views.league, name="league"),
     path("about/", views.about, name="blog-about"),
 ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,22 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Team, UserTeams
+from .models import Team, UserTeams, League
 
 # Create your views here.
-def home(request):
+def league(request, league_id):
     if request.method == "POST":
-        print(request.POST)
         if "create_team" in request.POST:
             # do stuff to create the team
             Team.objects.create(name=team1)
         elif "add_match" in request.POST:
             # do stuff to update team standings
-            print()
+            leagueName = request.POST["league"]
+            team1name = request.POST["team1"]
+            team1goals = request.POST["team1goals"]
+            team2name = request.POST["team2"]
+            team2goals = request.POST["team2goals"]
+
+            league = League.objects.get(id=league_id)
+            team1 = Team.objects.get(name=team1name, league=league)
+            team2 = Team.objects.get(name=team2name, league=league)
+
+            if team1goals > team2goals:
+                team1.wins += 1
+                team2.losses -= 1
+            elif team1goals < team2goals:
+                team2.wins += 1
+                team1.losses -= 1
+            else:
+                team1.draws += 1
+                team2.draws += 1
+
+            team1.goals_for += team1goals
+            team2.goals_for += team2goals
+            team1.goals_against += team2goals
+            team2.goals_against += team1goals
+
+            team1.save()
+            team2.save()
+
+            return True
+
         else:
             # unexpected error
             return HttpResponse("<h1>About</h1>")
 
-        team1 = request.POST["team1"]
     return render(request, "blog/select_league.html")
 
 
